ece834e471c352994295e45b781cc7f813cd7048
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2019-06-25 16:07:58 +0530

    Add files via upload

---
+++ SampleConApp/CollectionsExample.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using CommonLayer;
+/*
+* Collections is a group of classes created to represents various kinds of data strucures to suit UR App requirements which otherwise only depend or Arrays. These classes are created to remove the disadvantages of the arrays in its programs. Arrays are static in nature and fixed in size and U dont have possibility to either add items to the Array or remove items from the array without recreating it. 
+* .NET has 2 versions of collections: Generic and Non-Generic Version. Non-Generic version was the first form of Collections introduced in .NET 1.0. They are not type safe as they store the data as object. They are unsafe while reading the data. 
+* Generics was introduced in .NET 2.0. It is based on Template concept of C++ where the type is determined while creating the collection object. 
+*/
+namespace SampleConApp
+{
+    class CollectionsExample
+    {
+        static void Main(string[] args)
+        {
+            stackExample();
+            //queueExample();
+            //dictionaryExample();
+            //hashSetExample();
+            //listExample();
+            //nonGenericExample();
+        }
+
+        private static void stackExample()
+        {
+            Stack<string> pages = new Stack<string>();
+            do
+            {
+                Console.WriteLine("Enter the page to view");
+                pages.Push(Console.ReadLine());//Adds the item to the top....
+                string next = Prompt.GetString("Enter B for Back");
+                if (next == "B")
+                    pages.Pop();//Removes the top item from the Stack
+                foreach(string page in pages)
+                    Console.WriteLine(page);
+            } while (true);
+        }
+
+        private static void queueExample()
+        {
+            Queue<string> recentItems = new Queue<string>();
+            do
+            {
+                string item = Prompt.GetString("Enter the Item to see");
+                if (recentItems.Count == 5)
+                    recentItems.Dequeue();//Removes the first item in the queue. 
+                recentItems.Enqueue(item);//Adds the item to the bottomm
+                var list = recentItems.Reverse();//System.Linq...
+                Console.WriteLine("UR recently viewed items:");
+                foreach(string element in list)
+                    Console.WriteLine(element);
+            } while (true);
+        }
+
+        private static void dictionaryExample()
+        {
+            //memorywise its more in size, but highly optimized as U dont loop while accessing the members. Stores the data in the form of key-value pairs. Key is unique for the collection and value associated with the key may not be unique. Uniqueness of the key is based on the hashcode only. 
+            //username and password where username is unique but passwords for the users may or may not be the same. 
+            Dictionary<string, string> users = new Dictionary<string, string>();
+            users.Add("user1", "apple123");//Adds the pair to the dictionary
+            users["user2"] = "welcome123";//Another way to add the pair..
+            //Add method will throw an Exception if the Key already exists. [] operator will replace the value if the key exists, else adds the new key and the value to it. 
+            if (users.ContainsKey("user3"))//Checks for the key...
+                Console.WriteLine("User already exists");
+            else
+            {
+                Console.WriteLine("user does not exist\n Please enter a password");
+                users["user3"] = Console.ReadLine();
+            }
+            ///////////////display all the pairs from the dictionary////////////
+            foreach(KeyValuePair<string, string>pair in users)
+                Console.WriteLine($"{pair.Key}:{pair.Value}");
+            //Find a certain key and its value:
+            Console.WriteLine("The value of user1 is " + users["user1"]);//get the value of the specified key in the dictionary, no need to iterate....
+        }
+
+        private static void hashSetExample()
+        {
+            //HashSet is a collection class similar to List but stores only unique values in it. It implements an interface called ISet<T> which has methods to check the hash value of an object before it inserts to the collection. 
+            //HashSet maintains the Uniqueness if the class of the object overrides the Equals and the GetHashCode method. 
+            HashSet<Book> library = new HashSet<Book>();
+            library.Add(new Book { BookID = 111, Price = 456, Title = "Apple Nopl" });
+            library.Add(new Book { BookID = 112, Price = 456, Title = "Apple Nopl" });
+            library.Add(new Book { BookID = 113, Price = 456, Title = "Apple Nopl" });
+            library.Add(new Book { BookID = 114, Price = 456, Title = "Apple Nopl" });
+            library.Add(new Book { BookID = 115, Price = 456, Title = "Apple Nopl" });
+            Console.WriteLine("The Total books: " + library.Count);
+            //Add returns bool to tell whether the object is added or not...
+        }
+
+        private static void listExample()
+        {
+            List<string> fruits = new List<string>();//Blank List<T>
+            fruits.Add("Apple");//Adds to the bottom of the List....
+            fruits.Add("Mango");
+            fruits.Add("Banana");
+            fruits.Insert(2, "PineApple");//Anywhere in b/w the list if the index is within the bounds. 
+            foreach (String fruit in fruits) Console.WriteLine(fruit);
+            Console.WriteLine(fruits.Remove("Banana")); //Remove method tries to remove an existing object from the collection, if not found it returns false, else returns true. 
+            Console.WriteLine("The no items in the basket:" + fruits.Count);
+
+            //To display using for loop:
+            for (int i = 0; i < fruits.Count; i++)
+            {
+                Console.WriteLine(fruits[i]);//List supports INDEX also......
+            }
+            //List allows duplicates. It does not have any feature to allow only unique values within the list. 
+        }
+
+        private static void nonGenericExample()
+        {
+            //old Collections store the data as object, which is unsafe as the data type could be anything, they use a lot of boxing and unboxing which will reduce the performance of the Collection. 
+            ArrayList obj = new ArrayList();//Create Dynamic Arrays....
+            obj.Add(123);
+            obj.Add("Apple123");
+            for (int i = 0; i < obj.Count; i++)
+            {
+                Console.WriteLine(obj[i].ToString().ToUpper());
+            }
+        }
+    }
+}
---
+++ SampleConApp/ObjectUsage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+/*
+ * Customizing object type in C#.
+ * object is the base type for all the data types of .NET. Every class U create is derived from the object. object is a reference type. 
+ * object has 5 functions which can be overriden by the derived classes:
+ * ToString() method to convert the object to string. 
+ * Equals() method that defines the equality of an object with another. 
+ * GetHashCode() method is to get the Hashcode of the object. 
+ * MemberwiseClone() is a protected method that can be overriden to clone an object(Copy). 
+ * GetType method is used to get the Type information about the object. GetType method cannot be overriden. It is used only to get the data type details about the object.
+ * When an object is passed as an arg to the WriteLine method, it will evaluate the object to a string and displays the string. 
+ * By default, the fullname of the class is the string representation of it. However U could override the ToString method to get any string representation of the object like its data or a combination of its data.
+ * If U want to define the equality of 2 objects while comparing, then U should override the Equals method. it returns a bool where true means equality is successfull else they are unequal. The Equals method takes an object as an arg which is supposed to be the object to be compared with.  
+ */
+namespace SampleConApp
+{
+    class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+           if(obj is Book)
+            {
+                Book unboxed = obj as Book;
+                if ((Title == unboxed.Title) && (Price == unboxed.Price) && (BookID == unboxed.BookID))
+                    return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{Title} costs {Price:C}");
+        }
+
+        public override int GetHashCode()
+        {
+            //Its upto UR logic to return a 32 Bit no that identifies UR Object. 
+            return BookID.GetHashCode();
+        }
+    }
+    class ObjectUsage
+    {
+        static void Main(string[] args)
+        {
+            //toStringOverriding();
+            //equalsOverriding();
+            hashCodeOverriding();
+        }
+
+        private static void hashCodeOverriding()
+        {
+            //An hashcode is a numeric representation of the object to define its identity in a hash based collection. The HashCode is available for every variable or data type that u use in C#.
+            //HashCode is also used to equate 2 objects. If 2 Object's HashCode is different, then they are not equal and they dont belong to the same type. If the HashCode of 2 objects are same, then the CLR will go for the Equals method to check the equality of the objects,
+            //Hashbased collection used this hashCode to identify the objects. HashSet, HashTable, Dictionary, are the examples of the Collection classes that determine the identity of the objects in their collection thro GetHashCode method.
+            int x = 123;
+            int y = 234;
+            Console.WriteLine(x.GetHashCode());
+            Console.WriteLine(y.GetHashCode());
+            Console.WriteLine("Apple".GetHashCode());
+            Book bk1 = new Book { BookID = 123, Price = 550, Title = "2 States" };
+            Book bk2 = new Book { BookID = 123, Price = 550, Title = "2 States" };
+            Console.WriteLine(bk1.Equals(bk2));//true or false...
+        }
+
+        private static void equalsOverriding()
+        {
+            //2 reference type objects are equal if they share the same memory, same data does not guarantee the equality of the objects. 
+            Book bk1 = new Book { BookID = 123, Price = 550, Title = "2 States" };
+            Book bk2 = new Book { BookID = 124, Price = 550, Title = "2 States" };
+            if(bk1.Equals(bk2))
+                Console.WriteLine("They are same");
+            else
+                Console.WriteLine("They are different");
+        }
+
+        private static void toStringOverriding()
+        {
+            Book bk = new Book { BookID = 123, Title = "Professional C#", Price = 450 };
+            Console.WriteLine(bk);//Converting the object to a string and display the string, Its ToString method will do the job of converting it to string. 
+        }
+    }
+}
