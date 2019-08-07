b2bba018508305fcd45707b8823d249bfdf547a0
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2019-08-07 15:34:09 +0530

    Add files via upload

---
+++ MVVMExample/Models/StudentDB.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMExample.Models
+{
+    public class Student : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _id, _fees;
+        private string _name, _course;
+
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        public int StudentID
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                OnPropertyChanged("StudentID");
+            }
+        }
+
+        public string StudentName
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("StudentName");
+            }
+        }
+        public string StudentCourse
+        {
+            get { return _course; }
+            set
+            {
+                _course = value;
+                OnPropertyChanged("StudentCourse");
+            }
+        }
+
+        public int Fees
+        {
+            get { return _fees; }
+            set
+            {
+                _fees = value;
+                OnPropertyChanged("Fees");
+            }
+        }
+    }
+
+    public class StudentDatabase
+    {
+        private List<Student> _students = new List<Student>();
+        public void AddNewStudent(Student student)
+        {
+            _students.Add(student);
+        }
+
+        public void DeleteStudent(int id)
+        {
+            var student = _students.Find(s => s.StudentID == id);
+            if (student == null)
+                throw new Exception("Student not found to delete");
+            _students.Remove(student);
+        }
+
+        public void UpdateStudent(Student modifiedStudent)
+        {
+            var student = _students.Find(s => s.StudentID == modifiedStudent.StudentID);
+            if (student == null)
+                throw new Exception("Student not found to Update");
+            student.StudentCourse = modifiedStudent.StudentCourse;
+            student.StudentName = modifiedStudent.StudentName;
+            student.Fees = modifiedStudent.Fees;
+        }
+
+        public List<Student> GetAllStudents() => _students;
+    }
+}
---
+++ MVVMExample/ViewModels/StudentVM.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MVVMExample.Models;
+namespace MVVMExample.ViewModels
+{
+    //VM class should have Notification of Properties, Command Binding for Events. 
+    public class CustomCommand : ICommand
+    {
+        private Action<object> action;
+
+        public CustomCommand(Action<object> invoker)
+        {
+            action = invoker;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            //Logic to define whether the event should occur or not...
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //Code on which method to execute....
+            action(parameter);
+        }
+    }
+
+    public class StudentVM : INotifyPropertyChanged
+    {
+
+        private static StudentDatabase _db = new StudentDatabase();
+        public event PropertyChangedEventHandler PropertyChanged;
+        private Student newStudent = new Student();
+
+        private void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+        public Student NewStudent
+        {
+            get { return newStudent; }
+            set
+            {
+                newStudent = value;
+                OnPropertyChanged("NewStudent");
+            }
+        }
+        public ICommand AddCommand { get; set; }
+        public ICommand UpdateCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+
+        public StudentVM()
+        {
+            AddCommand = new CustomCommand(addFunc);
+            DeleteCommand = new CustomCommand(delFunc);
+            UpdateCommand = new CustomCommand(updateFunc);
+        }
+
+        public ObservableCollection<Student> AllStudents => new ObservableCollection<Student>(_db.GetAllStudents()); 
+
+        private void addFunc(object student)
+        {
+            if(student is Student)
+            {
+                var temp = new Student();
+                var copy = student as Student;
+                temp.StudentID = copy.StudentID;
+                temp.StudentName = copy.StudentName;
+                temp.StudentCourse = copy.StudentCourse;
+                temp.Fees = copy.Fees;
+                _db.AddNewStudent(temp);
+                NewStudent = new Student();
+            }
+            OnPropertyChanged("AllStudents");
+        }
+
+        private void delFunc(object id)
+        {
+            _db.DeleteStudent(int.Parse(id.ToString()));
+        }
+
+        private void updateFunc(object student)
+        {
+            if (student is Student)
+            {
+                _db.UpdateStudent(student as Student);
+            }
+        }
+    }
+}
