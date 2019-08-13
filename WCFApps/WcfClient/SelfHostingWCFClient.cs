8233a9910c288b611171494a8803cfdc38ec6399
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2019-08-13 16:53:15 +0530

    Add files via upload

---
+++ WCFApps/WcfClient/SelfHostingWCFClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ServiceModel;
+using WcfClient.myHostingServices;
+
+namespace SelfHostingClient
+{
+    class ClientProgram
+    {
+        static void Main(string[] args)
+        {
+            //readRecords();
+            addRecord();
+            //read Data from other machine using Http.....
+            //http://IPAddress:1234/MySelfHostServices/mex use this Address to access the clients....
+        }
+
+        private static void addRecord()
+        {
+            //net.tcp://localhost:4321/myTcpServices/mexTcp/
+            var tcpProxy = new WcfClient.myTcpServices.CustomerAddingServiceClient();
+            tcpProxy.AddNewCustomer(new WcfClient.myTcpServices.Customer
+            {
+                CustomerID = 201, CustomerAddress ="Bangalore", CustomerName="Phaniraj", CustomerPhone = 2342423455
+            });
+        }
+
+        private static void readRecords()
+        {
+            var proxy = new CustomerServiceClient();
+            var data = proxy.GetAllCustomers();
+            foreach (var cst in data) Console.WriteLine($"{cst.CustomerName} is from {cst.CustomerAddress}");
+        }
+    }
+}
---
+++ WCFApps/WebServiceClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using WebServiceClient.myWebServices;//use the namespace...
+namespace WebServiceClient
+{
+    //Set this Project as StartUp Project before U execute the Application...
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create the instance of the service object
+            EmpDataService empData = new EmpDataService();
+            var data = empData.GetAllRecords();//Call the method
+            var table = data.Tables[0];
+            foreach(DataRow row in table.Rows)
+                Console.WriteLine(row["EmpName"]);
+        }
+    }
+    //Advantages of  Web Services:
+    /*
+     * Accessible across multiple platforms. 
+     * A .NET Web service can be consumed by a Java App and a JSP Web service can be consumed by .NET App. 
+     * True Platform independent technology as data is transfered internally as XML.
+     */
+    /*
+     * Disadvantages of Web services:
+     * Only SOAP and HTTP, not usable for TCP 
+     * Only XML, or any data representable as XML can be used in Web services. 
+     * Later improvisations in Web technology could not be upgraded in Web services. 
+     * Multi Service Oriented Technologies like Web Services and .NET Remoting needed a common Framework for all kinds of Service based Applications leading to a new Framework called Windows Communication Foundation...
+     */
+}
